720faefa67de4b5c2972e72b1f2efbeee1b965f7
CapnWTF <marcus.morgan95@gmail.com>
2024-03-04 07:51:24 -0500

    first commit

---
+++ src/test/java/io/lightfeather/codingchallenge/codingChallenge/CodingChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package io.lightfeather.codingchallenge.codingChallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodingChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
